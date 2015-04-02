4dcd22f500eeacec14adc2e476471bf3e7cb343c
ethantanner <ethantanner@me.com>
2015-04-02 04:38:13 -0500

    Two added activities, ability to switch between streetview and mapview

--- app/src/main/java/edu/augustana/csc490/mapgame/MainActivity.java
+++ app/src/main/java/edu/augustana/csc490/mapgame/MainActivity.java
@@ -2,6 +2,7 @@ package edu.augustana.csc490.mapgame;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import android.location.Address;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -19,63 +23,34 @@ import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
 
-public class MainActivity extends Activity implements OnStreetViewPanoramaReadyCallback{
+public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.streetview);
+        setContentView(R.layout.activity_main);
+        Button startButton = (Button) findViewById(R.id.start);
+        startButton.setOnClickListener(startButtonListener);
 
-        StreetViewPanoramaFragment panoramaFragment = (StreetViewPanoramaFragment) getFragmentManager().findFragmentById(R.id.streetviewpanorama);
-        panoramaFragment.getStreetViewPanoramaAsync(this);
-
-    }
-
-    @Override
-    public void onStreetViewPanoramaReady(StreetViewPanorama panorama){
-        panorama.setPosition(playRound(), 5000);
-        panorama.setStreetNamesEnabled(false);
-    }
-
-    public LatLng playRound(){
-
-        Random r = new Random();
-
-        int zipcode = 0;
-
-        while(zipcode<90001 || zipcode>96162){
-            zipcode = r.nextInt(99999);
-        }
-
-        String stringZipCode = String.valueOf(zipcode);
-
-        return getLocationFromZipCode(stringZipCode);
 
     }
 
-    // code from stackoverflow.com/questions/3641304/get-latitude-and-longitude-using-zipcode
-    public LatLng getLocationFromZipCode(String zipCode){
 
-        final Geocoder geocoder = new Geocoder(this);
+    public View.OnClickListener startButtonListener = new View.OnClickListener() {
 
-        Log.w("geocoder zipcode input", zipCode);
+        @Override
+        public void onClick(View view) {
 
-        LatLng location = new LatLng(41.507844, -90.514576);
-
-        try{
-            List<Address> addresses = geocoder.getFromLocationName(zipCode,1);
-            if(addresses != null && !addresses.isEmpty()){
-                Address address = addresses.get(0);
-
-                location = new LatLng(address.getLatitude(),address.getLongitude());
-            }else{
-                Toast.makeText(this, "Geocode from zip error", Toast.LENGTH_LONG).show();
-            }
-        }catch(IOException e){
+            Intent intent = new Intent(MainActivity.this, StreetMode.class);
+            startActivity(intent);
 
         }
-        return location;
-    }
+    };
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -98,4 +73,7 @@ public class MainActivity extends Activity implements OnStreetViewPanoramaReadyC
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
